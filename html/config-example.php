b63ba82f17580a6fb333a6d31502da8c74fd4435
HumanJHawkins <HumanJHawkins@users.noreply.github.com>
2017-10-17 12:58:06 -0700

    Added UX lables to config.
    Signed-off-by: HumanJHawkins <HumanJHawkins@users.noreply.github.com>

--- html/config-example.php
+++ html/config-example.php
@@ -5,24 +5,24 @@
 // set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/include');
 
 // 'SITE_NAME' used as page heading, and/or whenever the full official site name is needed.
-$GLOBALS['SITE_NAME']       = 'Example Site';
+$GLOBALS['SITE_NAME'] = 'Example Site';
 
 // 'SITE_URL' is full official URL with no closing slash.
-$GLOBALS['SITE_URL']        = 'https://www.example.org';
+$GLOBALS['SITE_URL'] = 'https://www.example.org';
 
 // 'SITE_URL' is full official URL with no closing slash.
-$GLOBALS['SITE_DOMAIN']     = 'example.org';
+$GLOBALS['SITE_DOMAIN'] = 'example.org';
 
 // 'SITE_URL_CASUAL' is used to refer casually or informally to the site. i.e. "See you at example.com!"
 //   Usually this is the domain name.
 $GLOBALS['SITE_URL_CASUAL'] = $GLOBALS['SITE_DOMAIN'];
 
 // Website Base Path.
-$GLOBALS['WEB_BASE_DIRECTORY']        = '/var/www/www.example.org/html/';
+$GLOBALS['WEB_BASE_DIRECTORY'] = '/var/www/www.example.org/html/';
 
 // Content store path base. File uploads for user contributed content go here.
 // For security, keep this out of directories that are served by your web server.
-$GLOBALS['CONTENT_STORE_DIRECTORY']   = '/var/www/www.example.org/userContent/';
+$GLOBALS['CONTENT_STORE_DIRECTORY'] = '/var/www/www.example.org/userContent/';
 
 // Password encryption is necessarily and purposefully costly as far as processor time. A balance must be struck
 //  between making it expensive for hackers to crack, and making it inexpensive enough for the site to function.
@@ -30,40 +30,42 @@ $GLOBALS['CONTENT_STORE_DIRECTORY']   = '/var/www/www.example.org/userContent/';
 // Typically you want your password decryption to take less than 50ms. To translate "cost" to milliseconds on
 //  your hardware, include functions.php and run getSaltHashTimeCost($cost). NOTE: Increase slowly when testing
 //  and be careful with numbers over about 12.
-$GLOBALS['PASSWORD_HASH_COST']   = 8;
+$GLOBALS['PASSWORD_HASH_COST'] = 8;
 $GLOBALS['VERIFYCODE_HASH_COST'] = 5;
 
 // The debugOut function makes heavy use of this. It's very helpful during debugging to
 //  live monitor this. To do so, SSH into your server and use:
 //    tail -f var/log/nginx/dev.log
 // This file must exist. Create the file and set permissions to allow PHP to write to it..)
-$GLOBALS['LOG_FILE_PATH']    = '/var/log/nginx/dev.log';
+$GLOBALS['LOG_FILE_PATH'] = '/var/log/nginx/dev.log';
 
 // DB Connection info.
-$GLOBALS['DB_SERVER']        = 'DB Domain Name or Address';
-$GLOBALS['DB_USERNAME']      = 'DB Username';
-$GLOBALS['DB_PASSWORD']      = 'DB Password';
-$GLOBALS['DB_DATABASE']      = 'Database (Schema)';
-$GLOBALS['DB_PORT']          = 'Port (Usually 3306)';
+$GLOBALS['DB_SERVER'] = $GLOBALS['SITE_DOMAIN'];
+$GLOBALS['DB_USERNAME'] = 'DB Username';
+$GLOBALS['DB_PASSWORD'] = 'DB Password';
+$GLOBALS['DB_DATABASE'] = 'Database (Schema)';
+$GLOBALS['DB_PORT'] = '3306';
 $GLOBALS['DB_CHARACTER_SET'] = 'utf8';
-$GLOBALS['DB_OPTIONS']       = [
-  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$GLOBALS['DB_DSN'] = "mysql:host=" . $GLOBALS['DB_SERVER'] . ";dbname=" . $GLOBALS['DB_DATABASE'] . ";" .
-  "port=" . $GLOBALS['DB_PORT'] .";charset=". $GLOBALS['DB_CHARACTER_SET'];
-
+$GLOBALS['DB_OPTIONS'] =
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false,];
+$GLOBALS['DB_DSN'] =
+    "mysql:host=" . $GLOBALS['DB_SERVER'] . ";dbname=" . $GLOBALS['DB_DATABASE'] . ";" . "port=" . $GLOBALS['DB_PORT'] .
+    ";charset=" . $GLOBALS['DB_CHARACTER_SET'];
 
 // Mailgun connection info.
-$GLOBALS['MAILGUN_API_KEY']              = 'Mailgun API Key';
-$GLOBALS['MAILGUN_MAIL_DOMAIN']          = 'Mailgun Mail Domain';
+$GLOBALS['MAILGUN_API_KEY'] = 'Mailgun API Key';
+$GLOBALS['MAILGUN_MAIL_DOMAIN'] = 'Mailgun Mail Domain';
 
 // UI for contact verification.
-$GLOBALS['VERIFICATION_EMAIL_FROM']      = 'verify@' . $GLOBALS['SITE_DOMAIN'];
-$GLOBALS['VERIFICATION_EMAIL_SUBJECT']   = 'Please Verify Your Email';
-$GLOBALS['VERIFICATION_EMAIL_SIGNATURE'] = 'The '. $GLOBALS['SITE_NAME'] . 'Community';
+$GLOBALS['VERIFICATION_EMAIL_FROM'] = 'verify@' . $GLOBALS['SITE_DOMAIN'];
+$GLOBALS['VERIFICATION_EMAIL_SUBJECT'] = 'Please Verify Your Email';
+$GLOBALS['VERIFICATION_EMAIL_SIGNATURE'] = 'The ' . $GLOBALS['SITE_NAME'] . ' Community';
 
+// UI Lables
+$GLOBALS['CONTENT_THUMBNAIL_LABEL'] = 'Content Graphic';
+$GLOBALS['CONTENT_SUMMARY_LABEL'] = 'Summary';
+$GLOBALS['CONTENT_EXCERPT_LABEL'] = 'Sample';
+$GLOBALS['CONTENT_DESCRIPTION_LABEL'] = 'Description';
 
 // Development
 ini_set('display_errors', 1);
