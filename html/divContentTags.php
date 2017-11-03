b0c28df03630d3f96778ee903a7052f2dfb3a56d
HumanJHawkins <HumanJHawkins@users.noreply.github.com>
2017-11-03 11:45:32 -0700

    Filtered empty results from tags display.
    Signed-off-by: HumanJHawkins <HumanJHawkins@users.noreply.github.com>

--- html/divContentTags.php
+++ html/divContentTags.php
@@ -5,7 +5,7 @@ consolidatePageContentID();
 if (!isset($pdo)) {
   $pdo = getDBPDO();
 }
-  $sql = 'CALL procGetContentTags(?, ?)';
+$sql = 'CALL procGetContentTags(?, ?)';
 
 if ((isset($_SESSION['userID'])) && ($_SESSION['userID'] > 0)) {
   $sqlParamArray = [$_POST["pageContentID"], $_SESSION['userID']];
@@ -15,24 +15,33 @@ if ((isset($_SESSION['userID'])) && ($_SESSION['userID'] > 0)) {
 
 $currentTagCategory = '';
 $result = getOnePDOTable($pdo, $sql, $sqlParamArray, PDO::FETCH_ASSOC);
-
+debugOut('******************************************************************************************************************************');
+debugOut('******************************************************************************************************************************');
+debugOut('******************************************************************************************************************************');
+debugOut('******************************************************************************************************************************');
+debugOut('******************************************************************************************************************************');
+outputArray($result);
 echo '<div id="contentTags">';
-foreach($result as $key => $value) {
-  if ($currentTagCategory != $value['tagCategory']) {
-    $currentTagCategory = $value['tagCategory'];
-    echo '<br /><strong>' . $value['tagCategory'] . ':</strong> ';
-    if ($value['canEdit']) {
-      // First one has different punctuation, so keep inside the conditional
-      echo '<a href = "" class="btn btn-default btn-xs" onclick="return confirm(\'Remove this tag?\');">' . $value['tag'] . '&nbsp;&#9745;</a>';
+foreach ($result as $key => $value) {
+  if ($value['tag'] != '') {
+    if ($currentTagCategory != $value['tagCategory']) {
+      $currentTagCategory = $value['tagCategory'];
+      echo '<br /><strong>' . $value['tagCategory'] . ':</strong> ';
+      if ($value['canEdit']) {
+        // First one has different punctuation, so keep inside the conditional
+        echo '<a href = "" class="btn btn-default btn-xs" onclick="return confirm(\'Remove this tag?\');">' .
+            $value['tag'] . '&nbsp;&#9745;</a>';
+      } else {
+        echo $value['tag'];
+      }
     } else {
-      echo $value['tag'];
-    }
-  } else {
-    if ($value['canEdit']) {
-      // First one has different punctuation, so keep inside the conditional
-      echo '&nbsp;<a href = "" class="btn btn-default btn-xs" onclick="return confirm(\'Remove this tag?\');">' . $value['tag'] . '&nbsp;&#9745;</a>';
-    } else {
-      echo ', ' . $value['tag'];
+      if ($value['canEdit']) {
+        // First one has different punctuation, so keep inside the conditional
+        echo '&nbsp;<a href = "" class="btn btn-default btn-xs" onclick="return confirm(\'Remove this tag?\');">' .
+            $value['tag'] . '&nbsp;&#9745;</a>';
+      } else {
+        echo ', ' . $value['tag'];
+      }
     }
   }
 }
